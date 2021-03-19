bdc6296856833b9d191070da156383d02a975501
Martha Paola JImenez Martinez <martha.jimenez@islas.org.mx>
2021-03-19 16:13:17 -0700

    👷 Agrega prueba para add_three

--- tests/test_calculate_eda_snakes.py
+++ tests/test_calculate_eda_snakes.py
@@ -13,3 +13,9 @@ def test_add_two():
     expected_value = 3
     obtained_value = add_two(1)
     assert expected_value == obtained_value
+
+
+def test_add_three():
+    expected_value = 4
+    obtained_value = add_three(1)
+    assert expected_value == obtained_value
