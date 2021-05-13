aed264b0aea7308221faa1bc86bf3fd766106b35
Martha Paola JImenez Martinez <martha.jimenez@islas.org.mx>
2021-05-13 14:26:55 -0700

    📝 Agrega una prueba de la funcion create_box_plot_data()

--- tests/test_calculate_eda_snakes.py
+++ tests/test_calculate_eda_snakes.py
@@ -1,5 +1,5 @@
 from mi_modulo import *
-
+import numpy as np
 
 def test_nothing():
     pass
@@ -19,3 +19,12 @@ def test_add_three():
     expected_value = 4
     obtained_value = add_three(1)
     assert expected_value == obtained_value
+
+def test_create_box_plot_data():
+    df_test = pd.DataFrame({"Temporada": [2000,2001,2002,2001], "Longitud": [10,20,30,40]})
+    obtained_box_plot_data, obtained_seasons = create_box_plot_data(df_test,"Longitud")
+    expected_box_plot_data = [10,20,30]
+    expected_seasons = [2000,2001,2002]
+    np.testing.assert_array_equal(obtained_seasons,expected_seasons) 
+    np.testing.assert_array_equal(obtained_box_plot_data,expected_box_plot_data) 
+
