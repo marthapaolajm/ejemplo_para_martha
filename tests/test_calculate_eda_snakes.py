400d74491b4e76045424ca751451b2b6a1390c0c
Martha Paola JImenez Martinez <martha.jimenez@islas.org.mx>
2021-05-14 12:58:57 -0700

    💥Corrige lista de series de pandas e index

--- tests/test_calculate_eda_snakes.py
+++ tests/test_calculate_eda_snakes.py
@@ -24,8 +24,8 @@ def test_add_three():
 def test_create_box_plot_data():
     df_test = pd.DataFrame({"Temporada": [2000,2001,2002,2001], "Longitud": [10,20,30,40]})
     obtained_box_plot_data, obtained_seasons = create_box_plot_data(df_test,"Longitud")
-    expected_box_plot_data = [10,20,30]
+    expected_box_plot_data = [pd.Series(10,index=[0], name="Longitud"),pd.Series([20,40],index=[1,3], name="Longitud"),pd.Series(30,index=[2], name="Longitud")]
     expected_seasons = [2000,2001,2002]
     np.testing.assert_array_equal(obtained_seasons,expected_seasons) 
-    np.testing.assert_array_equal(obtained_box_plot_data,expected_box_plot_data) 
-
+    for i in range(3):
+        pd.testing.assert_series_equal(obtained_box_plot_data[i], expected_box_plot_data[i])
