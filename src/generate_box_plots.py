71e34b34b1b7ead67620703d28795c556660e64a
Martha Paola JImenez Martinez <martha.jimenez@islas.org.mx>
2021-05-27 11:14:35 -0700

    🔧Agrega fig en la funcion generate_box_plots

--- src/generate_box_plots.py
+++ src/generate_box_plots.py
@@ -16,7 +16,7 @@ for feature in lista_de_variables:
 
     boxsplotdata, seasons = create_box_plot_data(data_feature, feature)
     fig, ax = create_box_plot(boxsplotdata)  #graf + grande y se remueva barras
-    set_box_plot_style(ax,data_feature[feature],seasons)
+    set_box_plot_style(ax,data_feature[feature],seasons,fig)
     set_axis_labels(ax,feature)
     
     plt.savefig(f"reports/figures/diagrama_cajas_{feature}_serpientes_isabel.png", transparent=True, dpi=300)  
