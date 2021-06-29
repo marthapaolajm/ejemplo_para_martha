b0317beae5ff86232bb9b31a5dc04b562ef538d6
Martha Paola JImenez Martinez <martha.jimenez@islas.org.mx>
2021-06-29 13:15:12 -0700

    ✅ Agrega prueba de set_axis_labels

--- tests/test_calculate_eda_snakes.py
+++ tests/test_calculate_eda_snakes.py
@@ -22,6 +22,24 @@ def test_add_three():
     obtained_value = add_three(1)
     assert expected_value == obtained_value
 
+def test_set_axis_labels():
+    fig, expected_ax_one = geci_plot()
+    fig, expected_ax_two = geci_plot()
+    fig, obteined_ax_one = geci_plot()
+    fig, obteined_ax_two = geci_plot()
+    variable_uno = "Masa_del_individuo"
+    variable_dos = "Longitud_Total"
+    expected_ax_one.set_ylabel("Masa del individuo (gr)", fontsize=25, labelpad=10)
+    expected_value_gr = expected_ax_one.get_ylabel()
+    set_axis_labels(obteined_ax_one,variable_uno)
+    obtained_value_gr = obteined_ax_one.get_ylabel()
+    expected_ax_two.set_ylabel("Longitud Total (cm)", fontsize=25, labelpad=10)
+    expected_value_cm = expected_ax_two.get_ylabel()
+    set_axis_labels(obteined_ax_two,variable_dos)
+    obtained_value_cm = obteined_ax_two.get_ylabel()
+    assert expected_value_gr == obtained_value_gr
+    assert expected_value_cm == obtained_value_cm
+
 def test_create_box_plot_data():
     df_test = pd.DataFrame({"Temporada": [2000,2001,2002,2001], "Longitud": [10,20,30,40]})
     obtained_box_plot_data, obtained_seasons = create_box_plot_data(df_test,"Longitud")
@@ -31,7 +49,7 @@ def test_create_box_plot_data():
     for i in range(3):
         pd.testing.assert_series_equal(obtained_box_plot_data[i], expected_box_plot_data[i])
 
-@pytest.mark.mpl_image_compare(tolerance=0)
+@pytest.mark.mpl_image_compare(tolerance=0, savefig_kwargs={'dpi':300})
 def test_set_box_plot_style():
     diccionario = {"A": [10,22,30,44,10.1,20.2,30.3,40.4], "Temporada":[2013,2013,2013,2013,2015,2015,2015,2015]}
     data_feature = pd.DataFrame(diccionario)
@@ -41,3 +59,14 @@ def test_set_box_plot_style():
     fig, ax = Graficador.set_box_plot_style(data_feature[feature],seasons)
     set_axis_labels(ax, "Masa_del_individuo")
     return fig
+
+# @pytest.mark.mpl_image_compare(tolerance=0)
+# def test_set_ticks():
+# diccionario = {"A": [10,22,30,44,10.1,20.2,30.3,40.4], "Temporada":[2013,2013,2013,2013,2015,2015,2015,2015]}
+# data_feature = pd.DataFrame(diccionario)
+# feature = "A"
+# boxsplotdata, seasons = create_box_plot_data(data_feature, feature)
+# Graficador = Plotter(boxsplotdata)  #graf + grande y se remueva barras
+# fig, ax = Graficador.set_ticks()
+# set_ticks()
+# return fig
